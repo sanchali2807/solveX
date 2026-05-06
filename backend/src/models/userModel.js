f1011b709ba2a8797e846bd4cfe9842aef120f5f
Sanchali <sanchali280705@gmail.com>
2026-05-06 18:57:00 +0530

    done with global error middleware

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -48,3 +48,11 @@ const userSchema = new mongoose.Schema({
     }
 )
 
+// hash password before save 
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")){
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
