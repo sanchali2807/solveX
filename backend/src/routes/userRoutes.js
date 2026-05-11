eca378731219b7ac1141d43e3d1c99eef5f37b13
Sanchali <sanchali280705@gmail.com>
2026-05-11 18:34:38 +0530

    update password feature

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {getMyProfile , adminDashBoard , updateProfile} = require("../controllers/userController")
+const {getMyProfile , adminDashBoard , updateProfile,updatePassword} = require("../controllers/userController")
 const {protect , authorizeRole} = require("../middleware/authMiddleware");
 
 router.get("/me" ,protect,getMyProfile);
 router.get("/admin" ,protect,authorizeRole("admin"),adminDashBoard)
 router.put("/update-profile",protect,updateProfile);
+router.put("/updatePassword",protect,updatePassword);
 
 module.exports = router;
